a67a61f15a76ac2b85b1f5ae3d0787adc0ee06ae
Gonzalo Odiard <godiard@gmail.com>
2015-07-07 17:47:33 -0300

    Remove temporary ant build files from git

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/gonzalo/sugar-devel/honey2/find-words-cordova.git/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/gonzalo/sugar-devel/honey2/find-words-cordova.git/platforms/android/CordovaLib/AndroidManifest.xml \
